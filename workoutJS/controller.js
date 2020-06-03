7d6dde97a2052903701bd8249ff663c2495b75af
Eitan Behar <eitan.behar@allscripts.coom>
2020-06-03 17:54:39 +0300

    Add kettlebell and fix sets for lazy mode

--- workoutJS/controller.js
+++ workoutJS/controller.js
@@ -2,14 +2,14 @@ app.controller("controller", function($scope, $http) {
     
     $scope.moods = [ "Normal", "Lazy", "Energetic"];
     $scope.selectedMode = "Normal";
-    $scope.workoutOptions = { extra: true, bodyweight: true, barbell: true, aerobic: true}
+    $scope.workoutOptions = { extra: true, bodyweight: true, barbell: true, kettlebell: true, aerobic: true}
 
     $scope.generateWorkout = function(){        
         $http.get('workouts.json').then(function(response) {
             
             let drills = [];
             
-            drills.push(SelectDrill(response.data.modes, null));
+            drills.push(SelectDrill(response.data.modes, $scope.selectedMode, null));
 
             if($scope.workoutOptions.extra == true)
                 drills.push(SelectDrill(response.data.workouts.extra, $scope.selectedMode, $scope.workoutOptions));
@@ -34,16 +34,18 @@ app.controller("controller", function($scope, $http) {
         if(options.barbell == true)        
             filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "barbell"; }));        
         if(options.bodyweight == true)
-        filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "bodyweight"; }));        
+            filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "bodyweight"; }));        
         if(options.aerobic == true)
-        filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "aerobic"; }));        
+            filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "aerobic"; }));        
+        if(options.kettlebell == true)
+            filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "kettlebell"; }));        
     }
 
     let drill = filtered[Math.floor(Math.random() * filtered.length)];
     if(mood == "Lazy")
-        drill.limit = Math.round(parseInt(drill.limit) * 0.4);
+        drill.limit = Math.round(parseInt(drill.limit) * 0.8);        
     if(mood == "Energetic")
-        drill.limit = Math.round(parseInt(drill.limit) * 1.6);
+        drill.limit = Math.round(parseInt(drill.limit) * 1.2);    
 
     return drill;
   }
