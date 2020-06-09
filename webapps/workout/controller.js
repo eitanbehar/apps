a95094df828704a3959938eaea536ffa5ea0589e
eitan behar <eitan@baconao.net>
2020-06-09 18:45:00 +0300

    Add drills

--- webapps/workout/controller.js
+++ webapps/workout/controller.js
@@ -1,63 +1,59 @@
-app.controller("controller", function($scope, $http) {
-    
-    $scope.moods = [ "Normal", "Lazy", "Energetic"];
-    $scope.selectedMode = "Normal";
-    $scope.workoutOptions = { extra: true, bodyweight: true, barbell: true, kettlebell: true, aerobic: true}
+app.controller("controller", function ($scope, $http) {
 
-    $scope.generateWorkout = function(){        
-        $http.get('workouts.json').then(function(response) {
-            
+    $scope.moods = ["Normal", "Go Easy", "Energetic"];
+    $scope.selectedMode = "Normal";
+    $scope.workoutOptions = { extra: true, bodyweight: true, barbell: true, kettlebell: true, aerobic: true }
+
+    $scope.generateWorkout = function () {
+        $http.get('workouts.json').then(function (response) {
+
             let drills = [];
-            
+
             drills.push(SelectDrill(response.data.modes, $scope.selectedMode, null));
 
-            if($scope.workoutOptions.extra == true)
+            if ($scope.workoutOptions.extra == true)
                 drills.push(SelectDrill(response.data.workouts.extra, $scope.selectedMode, $scope.workoutOptions));
 
             drills.push(SelectDrill(response.data.workouts.upper, $scope.selectedMode, $scope.workoutOptions));
-            drills.push(SelectDrill(response.data.workouts.core,$scope.selectedMode, $scope.workoutOptions));
+            drills.push(SelectDrill(response.data.workouts.core, $scope.selectedMode, $scope.workoutOptions));
             drills.push(SelectDrill(response.data.workouts.lower, $scope.selectedMode, $scope.workoutOptions));
             $scope.drills = drills;
 
-         });
+        });
     }
-  });
+});
 
-  function SelectDrill(drillArray, mood, options)
-  {
+function SelectDrill(drillArray, mood, options) {
     let filtered = [];
-    if(options == null) 
-    {     
+    if (options == null) {
         filtered = drillArray;
-    }    
-    else
-    {
-        if(options.barbell == true)        
-            filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "barbell"; }));        
-        if(options.bodyweight == true)
-            filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "bodyweight"; }));        
-        if(options.aerobic == true)
-            filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "aerobic"; }));        
-        if(options.kettlebell == true)
-            filtered = filtered.concat(drillArray.filter(function(x) { return x.type == "kettlebell"; }));        
+    }
+    else {
+        if (options.barbell == true)
+            filtered = filtered.concat(drillArray.filter(function (x) { return x.type == "barbell"; }));
+        if (options.bodyweight == true)
+            filtered = filtered.concat(drillArray.filter(function (x) { return x.type == "bodyweight"; }));
+        if (options.aerobic == true)
+            filtered = filtered.concat(drillArray.filter(function (x) { return x.type == "aerobic"; }));
+        if (options.kettlebell == true)
+            filtered = filtered.concat(drillArray.filter(function (x) { return x.type == "kettlebell"; }));
     }
 
     let drill = filtered[Math.floor(Math.random() * filtered.length)];
-    if(mood == "Lazy")
-        drill.limit = Math.round(parseInt(drill.limit) * 0.8);        
-    if(mood == "Energetic")
-        drill.limit = Math.round(parseInt(drill.limit) * 1.2);   
-        
-    if(drill.vid == "")  
-    {        
-        drill.pic = "";
-        drill.url = "";
+    if (mood == "Go Easy")
+        drill.limit = Math.round(parseInt(drill.limit) * 0.8);
+    if (mood == "Energetic")
+        drill.limit = Math.round(parseInt(drill.limit) * 1.2);
+
+    if (drill != null) {
+        if (drill.vid == "") {
+            drill.pic = "";
+            drill.url = "";
+        }
+        else {
+            drill.pic = "https://img.youtube.com/vi/" + drill.vid + "/0.jpg";
+            drill.url = "https://www.youtube.com/watch?v=" + drill.vid;
+        }
     }
-    else
-    {
-        drill.pic = "https://img.youtube.com/vi/" + drill.vid  + "/0.jpg";
-        drill.url = "https://www.youtube.com/watch?v=" + drill.vid ;
-    }
-    
     return drill;
-  }
+}
