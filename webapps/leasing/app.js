fbeefcd1cd9ef8ebbdf6bbfb1e9b88c5e21b3f9b
eitan behar <eitan@baconao.net>
2020-06-26 14:11:18 +0300

    Add leasing compare app

--- webapps/leasing/app.js
+++ webapps/leasing/app.js
@@ -1,4 +1,10 @@
 var app = angular.module("app", []);
 app.controller("controller", function ($scope) {
   
+    $scope.fuel_price = 6.5;
+    $scope.km_per_year = 20000;
+    $scope.private = { price: 140000, km_per_liter: 15, insurance: 4500, rishui: 1500, tipul: 1000, devaluated: 90000 };
+    $scope.leasing_private = { initial: 8900, monthly: 2500, benefits_lost: 0 } ;
+    $scope.leasing_company = { initial: 0, monthly: 3200, benefits_lost: 700 } ;
+
 });
