db84e8978740561644264cf11a14a4edaf907554
eitan behar <eitan@baconao.net>
2020-07-07 14:43:11 +0300

    car cost app: minor updates

--- webapps/leasing/app.js
+++ webapps/leasing/app.js
@@ -3,7 +3,7 @@ app.controller("controller", function ($scope) {
   
     $scope.fuel_price = 6.5;
     $scope.km_per_year = 20000;
-    $scope.private = { price: 140000, km_per_liter: 15, insurance: 4500, rishui: 1500, tipul: 1000, devaluated: 90000 };
+    $scope.private = { price: 140000, km_per_liter: 15, insurance: 4500, rishui: 1500, tipul: 1000, devaluated: 35 };
     $scope.leasing_private = { initial: 8900, monthly: 2500, benefits_lost: 0 } ;
     $scope.leasing_company = { initial: 0, monthly: 3200, benefits_lost: 700 } ;
 
